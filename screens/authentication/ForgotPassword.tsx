310bcedb7ea001c07289b0f8e3e66fd63a7ea09d
blessdarah <blessdarahuba@gmail.com>
2021-09-06 16:35:26 +0100

    chore: add forgot password screen

---
+++ screens/authentication/ForgotPassword.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const ForgotPassword: React.FunctionComponent = () => {
+  return (
+    <View>
+      <
+    </View>
+  )
+}
+
+export default ForgotPassword
