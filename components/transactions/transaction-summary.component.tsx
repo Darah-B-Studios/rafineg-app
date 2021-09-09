849122827af36c8018609b5bd446cdc9b2e09438
BonyaO <bonyaoryn@gmail.com>
2021-09-09 05:33:08 +0100

    chore: fixed transactions summary on dashboard Added the date-fns package for date formatting

--- components/transactions/transaction-summary.component.tsx
+++ components/transactions/transaction-summary.component.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import tailwind from 'tailwind-rn'
 import { ITransaction } from '../../models/Transaction.model'
+import { format, compareAsc } from 'date-fns'
 
 
+type TransactionSummaryProps = {
+    transaction: ITransaction,
+    handlePress: () => void
+}
 
-
-const TransactionSummaryView: React.FunctionComponent<ITransaction>  = ({id =2, description, method, createOn, amount}) => {
+const TransactionSummaryView: React.FunctionComponent<TransactionSummaryProps>  = ({transaction, handlePress}) => {
+      const dateCreated =  format(transaction.createOn, 'dd MMM yyyy')
     return (
-        <View style={tailwind("flex-row justify-between py-2 border-gray-300 pb-2 border-b-2")}>
-            <Text style={tailwind("px-1")}>{description}</Text> 
-            <Text style={tailwind("px-1")}>{method}</Text>
-            <Text style={tailwind("px-1")}>{createOn.toDateString()}</Text>
-            <Text style={tailwind("px-1")}>{amount}</Text>
-        </View>
+        <TouchableOpacity onPress={handlePress} style={tailwind("flex-row justify-between my-2 p-2 border-gray-300 border-t-2 border-b-2")}>
+            <Text style={tailwind("px-1")}>{transaction.description}</Text> 
+            <Text style={tailwind("px-1")}>{transaction.method}</Text>
+            <Text style={tailwind("px-1")}>{dateCreated}</Text>
+            <Text style={tailwind("px-1")}>{transaction.amount}</Text>
+        </TouchableOpacity>
     )
 }
 
